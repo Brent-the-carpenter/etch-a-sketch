ce5c948a94d93b69b864ab992767f82b9d23477d
Brent-the-carpenter <Beh337@gmail.com>
2023-11-27 18:13:14 -0500

    made grid items turn black when mouseover

--- script.js
+++ script.js
@@ -2,19 +2,36 @@ const gridSize = 600;
 let rows = 16;
 let columns = 16;
 
+const pen = document.querySelector(".pen")
+
 const sketchPad = document.querySelector('.sketchpad');
 sketchPad.style.width = `${gridSize}px`;
 sketchPad.style.height = `${gridSize}px`;
 
 
-function makeGridCells(){
-  for (let i = 0; i < (rows * columns); i++ ){
-    const gridCell = document.createElement('div');
-    gridCell.classList.add('cell')
-    gridCell.style.width = `${(gridSize / rows) -2}px`;
-    gridCell.style.height = `${(gridSize / columns) -2}px`;
+    function changeBackgroundColor() {
+      this.style.backgroundColor = "black";
+    }
 
-    sketchPad.appendChild(gridCell);
-  }
-};
+    function makeGridCells(){
+      for (let i = 0; i < (rows * columns); i++ ){
+        const gridCell = document.createElement('div');
+        gridCell.classList.add('cell')
+        gridCell.style.width = `${(gridSize / rows) -2}px`;
+        gridCell.style.height = `${(gridSize / columns) -2}px`;
+
+        sketchPad.appendChild(gridCell);
+
+        
+        gridCell.addEventListener("mouseover" , changeBackgroundColor)
+          
+        
+      }
+      
+    }
+
+    
+
+    
+
 makeGridCells();
